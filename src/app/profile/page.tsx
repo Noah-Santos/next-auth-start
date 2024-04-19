2b11f9340da81feebeec679096a8107dd7d72794
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-04-19 16:45:37 -0700

    Practice
    Added a separate page for the profile and prevent the user from reaching the profile page without a session

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import styles from '../Styles/profile.module.css';
+import UserInfo from "@/components/UserInfo";
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from 'next/navigation'
 
-export default function Profile() {
+export default async function Profile() {
+  const authSession = await getServerAuthSession();
+
+  if(!authSession?.user){
+    redirect('/');
+  }
+
   return (
     <div>
-      
+      {authSession?.user && <UserInfo user={authSession?.user} />}
     </div>
   )
 }
